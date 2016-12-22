87718505ad6641ab862acae53eb1575522cc6874
Lukas Besch <connect@lukasbesch.com>
2016-12-22 16:32:44 +0100

    Add Vagrantfile and provisioners

---
+++ install-docker-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo su
+
+  curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+
+exit
